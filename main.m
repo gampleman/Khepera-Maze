41057c3b8f5c761fcf22dfac678c817a479a55d5
Jakub Hampl <s0802690@arkaig.inf.ed.ac.uk>
2010-11-10 13:21:53 +0000

    made the damn thing actually wokr

--- main.m
+++ main.m
@@ -5,13 +5,16 @@
   I1 = jacobean(I0, 400, 260, getroundobjects(~im2bw(I0), 0.5));
   % get green thing -> robot
   green_thresh = 200/255;
-  red_thresh = 40/255;
-[xs, ys] = find(I1(:, :, 2) > green_thresh & I1(:, :, 1) > red_thresh);
+  red_thresh = 100/255;
+[xs, ys] = find(I1(:, :, 2) > green_thresh & I1(:, :, 1) < red_thresh)
   robot = [mean(xs), mean(ys)]
     figure(1)
     imshow(I0)
     figure(2)
     imshow(I1)
+    hold on
+    text(robot(1), robot(2), 'Robot');
+    plot(robot(2), robot(1), 'ko');
     I2 = zeros(size(I1));
     I2(xs, ys, :) = 1;
     figure(3)
