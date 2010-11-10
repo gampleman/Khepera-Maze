919433af66da3ca633ddcc968aad540832a50c0e
Jakub Hampl <honitom@seznam.cz>
2010-11-10 16:27:37 +0000

    Added support for both end points

--- object_recognition/getendpoints.m
+++ object_recognition/getendpoints.m
@@ -1,8 +1,8 @@
-function End = getendpoints(I)
+function [End, Start] = getendpoints(I)
 %   GETENDPOINTS   Short description
 %     [[END, START]] = GETENDPOINTS(I)
 % 
-%   Gets the ending points in the image
+%   Gets the ending and beginening points in the image
 %   
 %   Created by Jakub Hampl on 2010-11-10.
 
@@ -13,5 +13,14 @@ obj = getroundobjects(~im2bw(I), 0.5);
 [e, ind] = min(obj(3, :));
 End = obj(1:2, ind);
 obj(3, ind) = 100000;
+
+if w == 6
+  [e, ind] = min(obj(3, :));
+  Start = obj(1:2, ind);
+  obj(3, ind) = 100000;
+end 
+
+
+
 
 end %  function
