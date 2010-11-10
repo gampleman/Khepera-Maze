970abfe714ae3b6cc9148af70b0587ecf848ed7a
Jakub Hampl <honitom@seznam.cz>
2010-11-10 11:57:51 +0000

    Updated main.m

--- main.m
+++ main.m
@@ -1,10 +1,13 @@
 while true
   % get image of the world
-  
+  I = take_snap();
   % use the jacobean transform
-  
+  I = jacobean(I, 260, 400, getroundobjects(~im2bw(I), 0.5));
   % get green thing -> robot
-  
+  green_thresh = 200;
+  red_thresh = 40;
+  [xs, ys] = find(I(:, :, 2) > green_thresh & I(:, :, 1) > red_thresh);
+  robot = [mean(xs), mean(ys)];
   % find the target point
   
   % plan path
