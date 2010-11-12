f815294367e24c63a3522487839a56fced9e86e8
Jakub Hampl <honitom@seznam.cz>
2010-11-12 10:34:02 +0000

    Refactored drivebot

--- movement/drivebot.m
+++ movement/drivebot.m
@@ -1,13 +1,23 @@
-function drivebot(speed, varargin)
-  numvarargs = length(varargin);
-  
-  if numvarargs > 1
-      error('drivebot:TooManyInputs requires at most 1 optional input');
+function drivebot(lspeed, varargin)
+%  DRIVEBOT(Left speed, right speed, pause)  
+%  DRIVEBOT(speed, pause)  
+%  DRIVEBOT(speed)  
+
+  switch length(varargin)
+    case 2
+      rspeed = varargin{1};
+      pausetime = varargin{2};
+    case 1
+      rspeed = lspeed;
+      pausetime = varargin{1};
+    case 0
+      rspeed = lspeed;
+      pausetime = 0;
+    otherwise
+      error('drivebot:TooManyInputs requires at most 2 optional inputs');
   end
   
-  send_command(strcat('D,', num2str(speed), ',', num2str(speed)));
+  send_command(strcat('D,', num2str(lspeed), ',', num2str(rspeed)));
   read_command;
-  
-  if numvarargs
-    pause(varargin{1});
-  end
+  pause(pausetime);
+
