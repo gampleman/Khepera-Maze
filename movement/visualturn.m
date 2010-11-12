285d237fa6f52594d924c58a203a979d69df7469
Jakub Hampl <honitom@seznam.cz>
2010-11-12 12:10:56 +0000

    I gave it a fighting chance

--- movement/visualturn.m
+++ movement/visualturn.m
@@ -3,6 +3,8 @@ function visualturn(old, new, dimension)
 a = old(dimension) - new(dimension)
 c = sqrt((old(1) - new(1))^2 + (old(2) - new(2))^2)
 
+other_dimension = 3 - dimension;
+
 alpha = acos(a / c);
 v = 10;
 
@@ -10,7 +12,12 @@ if alpha < 0.0873
   alpha = 0;
 end
 
-vl = v * (1 + (alpha - 1.5 * pi) / (0.5 * pi));
+
+if old(other_dimension) > new(other_dimension)
+  alpha = alpha * -1;
+end
+
+vl = v * (1 + alpha / (-0.5 * pi));
 vr = v * (1 + alpha / (0.5 * pi));
 
 drivebot(vl, vr, 0.5);
