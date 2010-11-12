fe184e9b6bafe5b40ff45a74721cbd18d650adc6
Jakub Hampl <honitom@seznam.cz>
2010-11-12 12:50:13 +0000

    WTF

--- movement/visualturn.m
+++ movement/visualturn.m
@@ -6,20 +6,24 @@ c = sqrt((old(1) - new(1))^2 + (old(2) - new(2))^2);
 other_dimension = 3 - dimension;
 if dimension == 1
   alpha = pi - acos(a / c);
+  if old(other_dimension) < new(other_dimension)
+    alpha = alpha * -1;
+  end
 else
   alpha = acos(a / c);
+  if old(other_dimension) > new(other_dimension)
+    alpha = alpha * -1;
+  end
 end
 v = 10;
 
-if alpha < 0.0873
+if alpha < 0.0873 || alpha > -0.0873
   alpha = 0;
   v = 20;
 end
 
 
-if old(other_dimension) > new(other_dimension)
-  alpha = alpha * -1;
-end
+alpha
 
 vl = v * (1 + alpha / (-0.5 * pi));
 vr = v * (1 + alpha / (0.5 * pi));
