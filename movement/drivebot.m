971b5ad13f02b70624b31e80af84cc02636ae571
Jakub Hampl <honitom@seznam.cz>
2010-11-16 10:44:10 +0000

    Updated drivebot to cap speeds

--- movement/drivebot.m
+++ movement/drivebot.m
@@ -16,8 +16,20 @@ function drivebot(lspeed, varargin)
     otherwise
       error('drivebot:TooManyInputs requires at most 2 optional inputs');
   end
-  if (lspeed < 40 || lspeed > -40) && (rspeed < 40 || rspeed > -40)
+  
+  if lspeed > 0
+    lspeed = min(40, lspeed);
+  else
+    lspeed = max(-40, lspeed);
+  end
+  
+  if rspeed > 0
+    rspeed = min(40, rspeed);
+  else
+    rspeed = max(-40, rspeed);
+  end
+  
     send_command(strcat('D,', num2str(round(lspeed)), ',', num2str(round(rspeed))));
     read_command; 
-  end
+
   pause(pausetime);
